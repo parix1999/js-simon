009e9f8aacc837d54dd38ad7450533ff498b42ce
parix1999 <parisottoedoardo@gmail.com>
2021-06-14 15:57:04 +0200

    js-2.0

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
 document.addEventListener('DOMContentLoaded', function (){
 
-    // Funzione per generare numeri random:
-    function randomNumber(min, max) {    
-        return Math.floor(Math.random() * (max - min +1) - min);
+    // genero un numero random da pushare in una array vuota:
+    numeriRandom = [];
+    // mi deve creare per 5 volte dei numeri random per poi pushare o meno
+    for (var i = 1; i <=5; i++) {
+        var num = parseInt(Math.floor(Math.random() * 6) + 1);
+        numeriRandom.push(num);
     }
-    console.log(randomNumber(1,50));
-    // inserisco in una variabile il random number cosi da poterlo utilizzare:
-    var casualNumber = randomNumber(1, 50);
+    console.log(numeriRandom); 
 
-    // Array vuota dove poi pushero i numeri generati casualmente dalla funzione
-    var numbers = [];
-    // Devo pushare dentro alla array i numeri con un ciclo:
-    while (numbers < 5) {
-        // se i numeri non sono presenti nell'array allora pusha sennò no:
-        if (!numbers.includes(casualNumber)) {
-            numbers.push(casualNumber);
-        }
-    }
-    console.log(numbers);
+
+
+    
 
 
 
