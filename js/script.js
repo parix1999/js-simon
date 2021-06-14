a9a983385e24794cc6b0bece911e7095a308826e
parix1999 <parisottoedoardo@gmail.com>
2021-06-14 16:27:13 +0200

    js-4.0

--- js/script.js
+++ js/script.js
@@ -2,12 +2,24 @@ document.addEventListener('DOMContentLoaded', function (){
 
     // genero un numero random da pushare in una array vuota:
     numeriRandom = [];
-    // mi deve creare per 5 volte dei numeri random per poi pushare o meno
-    for (var i = 1; i <=5; i++) {
-        var num = parseInt(Math.floor(Math.random() * 6) + 1);
-        numeriRandom.push(num);
+    // si genera i 5 numeri random da inserire della array:
+    for (var x = 0; numeriRandom.length<5; x++) {
+        // creazione della variabile per il random:
+        var randomNumber = parseInt(Math.floor(Math.random() * 6) +1 );
+        // Se il numero è già presente nella array non la inserisce
+        while (numeriRandom.length < 5){
+            if (!numeriRandom.includes(randomNumber)) {
+                numeriRandom.push(randomNumber);
+            }else{
+                break;
+            }
+        }
     }
-    console.log(numeriRandom); 
+    alert(numeriRandom);
+    
+        
+    
+
 
 
 
