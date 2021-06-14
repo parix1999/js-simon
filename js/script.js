7f3e534f96a1752b0a8988ab39bbedd4063b475a
parix1999 <parisottoedoardo@gmail.com>
2021-06-14 17:14:10 +0200

    js-7.0

--- js/script.js
+++ js/script.js
@@ -3,18 +3,15 @@ document.addEventListener('DOMContentLoaded', function () {
     // genero un numero random da pushare in una array vuota:
     numeriRandom = [];
     // si genera i 5 numeri random da inserire della array:
-    for (var x = 0; numeriRandom.length < 5; x++) {
-        // creazione della variabile per il random:
-        var randomNumber = parseInt(Math.floor(Math.random() * 6) + 1);
-        // Se il numero è già presente nella array non la inserisce
-        while (numeriRandom.length < 5) {
-            if (!numeriRandom.includes(randomNumber)) {
-                numeriRandom.push(randomNumber);
-            } else {
-                break;
-            }
+    // creazione della variabile per il random e
+    // Se il numero è già presente nella array non la inserisce
+    while (numeriRandom.length < 5) {
+        var randomNumber = parseInt(Math.floor(Math.random() * 100));
+        if (!numeriRandom.includes(randomNumber)) {
+            numeriRandom.push(randomNumber);
         }
     }
+
     var output = document.querySelector('.numbers');
     output.innerHTML = numeriRandom;
 
@@ -25,9 +22,9 @@ document.addEventListener('DOMContentLoaded', function () {
     // che deve succeder per un tot di volte dopo un tot di tempo
 
     var boxScomparsa = document.querySelector('.start');
-    setTimeout(function(){
+    setTimeout(function () {
         boxScomparsa.parentNode.removeChild(boxScomparsa);
-    },30000);
+    }, 30000);
 
 
 
