d2e1f7eefd34710f8c70281a462775532393f887
parix1999 <parisottoedoardo@gmail.com>
2021-06-14 17:48:27 +0200

    js-8.0

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // genero un numero random da pushare in una array vuota:
-    numeriRandom = [];
+    var numeriRandom = [];
+    var numeriUser = [];
+    var rightNumber = [];
     // si genera i 5 numeri random da inserire della array:
     // creazione della variabile per il random e
     // Se il numero è già presente nella array non la inserisce
@@ -13,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     var output = document.querySelector('.numbers');
-    output.innerHTML = numeriRandom;
+    alert('Memorizza questi 5 numeri' + ' ' + numeriRandom);
 
     /* Ora si crea il timer, che deve durare 30 secondi per poi far sparire l'alert
     quindi anche l'alert deve stare dentro al conto del tempo.
@@ -21,21 +23,26 @@ document.addEventListener('DOMContentLoaded', function () {
     // Userò il setTimeout perchè e qualcosa che succede dopo un tot di tempo e non
     // che deve succeder per un tot di volte dopo un tot di tempo
 
-    var boxScomparsa = document.querySelector('.start');
-    setTimeout(function () {
-        boxScomparsa.parentNode.removeChild(boxScomparsa);
-    }, 30000);
+    setTimeout(function(){
+        while (numeriUser.length < 5) {
+            var userNumber = parseInt(prompt('Inserisci i 5 numeri'));
+            numeriUser.push(numeriUser);
+            if (numeriRandom.includes(userNumber)) {
+                rightNumber.push(userNumber);
+            }
+        }
+        console.log('hai indovinato: ' + rightNumber + 'In totale quindi: ' +  ' ' + rightNumber.length);
+    },1000);
+
+    
 
 
 
 
-
-
-
-
-
-
-
+    // var boxScomparsa = document.querySelector('.start');
+    // setTimeout(function () {
+    //     boxScomparsa.parentNode.removeChild(boxScomparsa);
+    // }, 30000);
 
 
 
