8bcece757ddc691414701456d3905c394b203e63
parix1999 <parisottoedoardo@gmail.com>
2021-06-14 16:40:13 +0200

    js-5.0

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
 
     // genero un numero random da pushare in una array vuota:
     numeriRandom = [];
     // si genera i 5 numeri random da inserire della array:
-    for (var x = 0; numeriRandom.length<5; x++) {
+    for (var x = 0; numeriRandom.length < 5; x++) {
         // creazione della variabile per il random:
-        var randomNumber = parseInt(Math.floor(Math.random() * 6) +1 );
+        var randomNumber = parseInt(Math.floor(Math.random() * 6) + 1);
         // Se il numero è già presente nella array non la inserisce
-        while (numeriRandom.length < 5){
+        while (numeriRandom.length < 5) {
             if (!numeriRandom.includes(randomNumber)) {
                 numeriRandom.push(randomNumber);
-            }else{
+            } else {
                 break;
             }
         }
     }
-    alert(numeriRandom);
-    
-        
-    
+
+    /* Ora si crea il timer, che deve durare 30 secondi per poi far sparire l'alert
+    quindi anche l'alert deve stare dentro al conto del tempo.
+    */
+    // Userò il setTimeout perchè e qualcosa che succede dopo un tot di tempo e non
+    // che deve succeder per un tot di volte dopo un tot di tempo
+    document.addEventListener('click', function () {
+        setTimeout(myfunction(), 1000);
+        function myfunction() {
+            
+            
+        }
+
+    });
 
 
 
 
-    
+
+
+
 
 
 
